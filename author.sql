66eb0343fce9741bc602086015811def1f5bee79
tadashii1417 <tadashii1417@gmail.com>
2019-06-26 00:19:38 +0700

    add db

---
+++ author.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM library.authors
+LIMIT 0, 50000
+
+-- Date: 2019-06-26 00:18
+*/
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`phone`,`address`,`status`,`created`,`updated`) VALUES (1,'trường','dương','truong@gmail.com','09434334','Gò Vấp, HCM','active','2003-05-20 00:00:00','2005-12-25 00:00:00');
---
+++ book.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM library.books
+LIMIT 0, 50000
+
+-- Date: 2019-06-26 00:17
+*/
+INSERT INTO `` (`id`,`title`,`isbn`,`year`,`authorId`,`status`,`created`,`updated`,`view`,`vote`,`download`) VALUES (1,'Đường Cách mệnh','ABSN',1997,1,'còn','2018-08-22 00:00:00','2019-12-12 00:00:00',1,1,1);
+INSERT INTO `` (`id`,`title`,`isbn`,`year`,`authorId`,`status`,`created`,`updated`,`view`,`vote`,`download`) VALUES (2,'My way','123',1998,1,'hết','2019-01-01 00:00:00','2019-01-03 00:00:00',1,1,1);
